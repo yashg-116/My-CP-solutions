ee48d41ab360b051e6ce51ca8ff3c206a596747f
Yash Gupta <gyash6696@gmail.com>
2022-06-02 16:42:40 +0530

    Committing files

---
+++ CodeChef/C++14/GUESSIT/44025737.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	using namespace std; 
+	int j=0;
+	int n=0;
+   cin>>n;
+   for(int k=0;k<n;k++){
+   for (int i = 1; i <= 1000; ++i) 
+   { 
+      cout << i*i <<endl; 
+      cin>> j;
+      if(j==1){
+          break;
+      }
+            cout << flush;
+
+   } }
+   	return 0;
+}
