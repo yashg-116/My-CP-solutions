48d669732b3c92bd0195d2d890d75af2a0c2a3e4
Yash Gupta <gyash6696@gmail.com>
2022-06-02 16:42:39 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/IRSTXOR/43565843.py
@@ -0,0 +1,30 @@
+# cook your dish here
+
+def Binary(x):
+    return int(bin(x)[2:])
+n=int(input())
+
+for i in range(n):
+    num=int(input())
+    r=str(Binary(num))
+    t=0
+    n1=""
+    n2=""
+    for k in (r):
+        if k=="0":
+            n1=n1+"1"
+            n2=n2+"1"
+        else:
+            if t==0:
+                n1=n1+"1"
+                n2=n2+"0"
+                t=1
+            else:
+                n1=n1+"0"
+                n2=n2+"1"
+    print(int(n1,2)*int(n2,2))
+    
+    
+    
+
+
