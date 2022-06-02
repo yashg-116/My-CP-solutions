1e00d0859a74d6f9fcb8199af60c0f29faa61cc4
Yash Gupta <gyash6696@gmail.com>
2022-06-02 16:43:16 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1095C/157456121.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define ll long long
+void solve(){
+    ll n,k;
+    cin>>n>>k;
+    if(k>n) {cout<<"NO\n"; return;}
+    if( __builtin_popcount(n)>k) {cout<<"NO\n"; return;}
+    string s="";
+    queue<int> q;
+    map<int,int> m;int cnt=0;
+    while(n>0){
+        int x=n%2;
+        if(x==1){
+            if(cnt!=0) q.push(1<<cnt);
+            m[1<<cnt]++;
+        }
+        n>>=1;
+        cnt++;
+    }
+    cnt=m.size();
+    while(cnt<k){
+        int z = q.front();
+		q.pop();
+        m[z]--;
+        m[z / 2] += 2;
+		if(z / 2 > 1)
+		{
+            
+			q.push(z / 2);
+			q.push(z / 2);
+		}
+		cnt++;
+    }
+    cout<<"YES\n";
+    for(auto x : m)
+		for(int i = 0; i < x.second; i++)
+			cout<<x.first<<" ";
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+        solve();
+    return 0;
+}
