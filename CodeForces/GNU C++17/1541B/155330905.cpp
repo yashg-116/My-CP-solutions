c96ff124ad38dbd450ddf3deef4fe934fa3628ca
Yash Gupta <gyash6696@gmail.com>
2022-06-11 11:15:13 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1541B/155330905.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        ll a[n+1];
+        for(int i=1;i<=n;i++){
+            cin>>a[i];
+        }
+        ll cnt=0;
+        for(int i=1;i<=n;i++){
+            ll m=a[i];
+            ll k=a[i]-i;
+            for(k;k<=n;k+=m){
+                if(k>0 && i<k)
+                    if(m*a[k]==(i+k)) cnt++;
+            }
+        }
+        cout<<cnt<<'\n';
+    }
+}
